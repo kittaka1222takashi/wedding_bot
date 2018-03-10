c55ec3ca91c6f61e9bf2f2ef85f50a5abbf67843
菊池喬 <kittaka.takashi@gmail.com>
2018-03-10 22:53:45 +0900

    atest

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ def handle_message(event):
                 # TextSendMessage(text=event.message.text),
                 # TextSendMessage(text=util.get_message(event.message.text)),
                 TextSendMessage(text="式中に撮った写真を送って下さい(*^^*)"),
-                TextSendMessage(text="送っていただいた写真は新郎新婦や参列者の皆様にシェアします！"),
+                TextSendMessage(text="送っていただいた写真は後ほど新郎新婦や参列者の皆様にシェアします！"),
                 TextSendMessage(text="写真をいっぱい送ってくれた方には二次会のときにいいことがあるかも？"),
             ]
         )
