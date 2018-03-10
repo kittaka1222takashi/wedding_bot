edc34997fe11d20d892826e9e8175e8adb5e61c4
菊池喬 <kittaka.takashi@gmail.com>
2018-03-10 22:25:07 +0900

    atest

--- app.py
+++ app.py
@@ -137,7 +137,10 @@ def handle_message(event):
 
         carousel_template_message = TemplateSendMessage(
             alt_text='Saved Picture',
-            template=CarouselTemplate(columns=img_columns)
+            template=CarouselTemplate(
+                columns=img_columns,
+                imageSize="contain"
+            )
         )
         line_bot_api.reply_message(
             event.reply_token,
