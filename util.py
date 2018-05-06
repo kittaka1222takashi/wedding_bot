b2b6d4211addebfa680f49b5d7f0806cbb9a27e0
菊池喬 <kittaka.takashi@gmail.com>
2018-05-06 18:00:01 +0900

    modify

--- util.py
+++ util.py
@@ -41,6 +41,6 @@ def get_ranking(dbx, line_bot_api):
         profile = line_bot_api.get_profile(user_id)
         ranking.append([rank, user_id, file_num_sorted[user_id], profile.display_name])
         message = message + str(rank) + "位 " + profile.display_name + "さん " + str(num) + "枚\n" + user_id + "\n"
-    message = "です！"
+    message = message + "です！"
     print(ranking)
     return message
